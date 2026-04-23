e69ba90db06296331999de281b7b64b256f37720
timooooo414 <timo.hoeglinger@tfs-haslach.at>
2026-04-23 09:43:33 +0200

    main menu click funktionnnn

--- src/game/scenes/MainMenu.js
+++ src/game/scenes/MainMenu.js
@@ -3,23 +3,52 @@ import { Scene } from "phaser";
 export default class MainMenu extends Scene {
 
     text;
-
+    start = null;
     constructor() {
-        super('MainMenu');
+        super('mainmenu');
     }
 
     preload(){
 
     }
 
+    input(){
+        
+    }
+
     create() {
 
         console.log('MainMenu create called');
 
-        this.text = this.add.text(this.scale.width / 2, this.scale.height / 2, 'Main Menu', { fontFamily: 'Arial', fontSize: 64, color: '#00ff00' }).setOrigin(0.5);
+        this.MainMenu = this.add.text(this.scale.width / 2, this.scale.height / 2, 'MainMenu', { fontFamily: 'Arial', fontSize: 64, color: '#00ff00' }).setOrigin(0.5);
 
-        if(true){
+        this.MainMenu.setInteractive();
+
+        let text = "";
+        let display = this.add.text(100, 100, "", { fontSize: "32px" });
+
+        this.input.keyboard.on("keydown", (event) => {
+            if (event.key === "Backspace") {
+                text = text.slice(0, -1);
+            } else {
+                text += event.key;
+            }
+            display.setText(text);
+            console.log('MainMenu ' , text ,' created');
+        });
+
+        
+
+        this.MainMenu.on('pointerdown', () => {
+            this.start = true;
+            console.log('MainMenu clicked');
+            console.log('start: ' + this.start); 
+
+        if(this.start === true){
             this.scene.start('MainGame');
         }
+        });
+
+
     }
 }
