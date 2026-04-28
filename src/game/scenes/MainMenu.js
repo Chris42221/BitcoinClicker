fd8ab23bdb17a81155d1129d2b6e78667c8e0d5b
timooooo414 <timo.hoeglinger@tfs-haslach.at>
2026-04-28 14:00:48 +0200

    ungültige eingabe

--- src/game/scenes/MainMenu.js
+++ src/game/scenes/MainMenu.js
@@ -24,20 +24,72 @@ export default class MainMenu extends Scene {
 
         this.MainMenu.setInteractive();
 
-        let text = "";
-        let display = this.add.text(100, 100, "", { fontSize: "32px" });
+        let username = "";
+        let password = "";
+        let usernameeingabe = true;
+        let passwordeingabe = false;
 
-        this.input.keyboard.on("keydown", (event) => {
-            if (event.key === "Backspace") {
-                text = text.slice(0, -1);
-            } else {
-                text += event.key;
-            }
-            display.setText(text);
-            console.log('MainMenu ' , text ,' created');
+
+
+        this.usernamebutton = this.add.text(700,200,"usernamebutton",{ fontFamily: 'Arial', fontSize: 32, color: '#00ff00', })
+        this.usernamebutton.setInteractive();
+        this.usernamebutton.on('pointerdown', () => {
+            this.usernameeingabe = true;
+            this.passwordeingabe = false;
+            console.log(username)
+        });
+        
+        this.passwordbutton = this.add.text(700,400,"passwordbutton",{ fontFamily: 'Arial', fontSize: 32, color: '#00ff00', })
+        this.passwordbutton.setInteractive();
+        this.passwordbutton.on('pointerdown', () => {
+            this.passwordeingabe = true;
+            this.usernameeingabe = false;
+            console.log(password)
         });
 
+
+
+
+
+
+        this.usernameScene = this.add.text(300,200,"username",{ fontFamily: 'Arial', fontSize: 32, color: '#00ff00', });
         
+        if(usernameeingabe == true){
+                this.input.keyboard.on("keydown", (event) => {
+                    if (event.key === "Backspace") {
+                        username = username.slice(0, -1);
+                    } else if(event.code.startsWith("Key")){
+
+                        username += event.key;
+                    }else {
+                        console.log("keine gültige taste")
+                    }
+                    this.usernameScene.setText(username);
+                    console.log('username: ' , username);
+                });
+        };
+
+
+
+
+        this.passwordScene = this.add.text(300,400,"password",{ fontFamily: 'Arial', fontSize: 32, color: '#00ff00' });
+
+        if(passwordeingabe == true){
+                this.input.keyboard.on("keydown", (event) => {
+                    if (event.key === "Backspace") {
+                        password = password.slice(0, -1);
+                    } else if(event.code.startsWith("Key")){
+
+                        password += event.key;
+                    }else {
+                        console.log("keine gültige taste")
+                    }
+                    console.log('passwort: ' , password );
+                });
+        };
+        
+
+
 
         this.MainMenu.on('pointerdown', () => {
             this.start = true;
@@ -50,5 +102,17 @@ export default class MainMenu extends Scene {
         });
 
 
+
     }
+
+    update(){
+
+
+
+
+
+
+
+    };
 }
+
